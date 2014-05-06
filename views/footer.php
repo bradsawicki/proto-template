806c9b419336fd04765e55b53db4bfd079946d85
Brad Sawicki <git@bradsawicki.com>
2014-05-06 10:48:49 -0500

    Switch to 2 space tabs init

--- views/footer.php
+++ views/footer.php
@@ -1,29 +1,29 @@
-<footer class="footer">
-	<div class="wrapper">
-
-		<div class="follow-us">
-			<ul class="social-icons">
-				<li><a href="" class="icon-facebook-circle">Facebook</a></li>
-				<li><a href="" class="icon-twitter-circle">Twitter</a></li>
-			</ul>
-		</div>
-
-		<nav class="footer-nav">
-		    <ul>
-		        <li><a href="/">Home</a></li>
-		        <li><a href="">Privacy</a></li>
-		        <li><a href="">Terms of Use</a></li>
-		        <li><a href="">Donate</a></li>
-		     </ul>
-		</nav>
-		
-		<div class="copyright">
-		    <p>Made by Brad Sawicki.</p>
-		    <p>Code for all, all for Code. Use what you want, bro.</p>
-		</div>
-
-	</div>
-</footer>
+  <footer class="footer">
+    <div class="wrapper">
+    
+      <div class="follow-us">
+        <ul class="social-icons">
+          <li><a href="" class="icon-facebook-circle">Facebook</a></li>
+          <li><a href="" class="icon-twitter-circle">Twitter</a></li>
+        </ul>
+      </div>
+      
+      <nav class="footer-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="">Privacy</a></li>
+          <li><a href="">Terms of Use</a></li>
+          <li><a href="">Donate</a></li>
+        </ul>
+      </nav>
+      
+      <div class="copyright">
+        <p>Made by Brad Sawicki.</p>
+        <p>Code for all, all for Code. Use what you want, bro.</p>
+      </div>
+    
+    </div>
+  </footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="/assets/js/scripts.js"></script>
--- views/header.php
+++ views/header.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Prototype | Company Name</title>
-	<link href="/assets/css/application.css" rel="stylesheet" media="all">
-    <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prototype | Company Name</title>
+  <link href="/assets/css/application.css" rel="stylesheet" media="all">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
 
-	<header class="header">
-		<div class="wrapper">
-			
-			<div class="logo">
-				<a href="./">Company Name</a>
-			</div>
-			
-			<nav class="main-nav">
-    			<a href="" class="btn mobile-menu-btn">Menu <span class="icon-menu"><span class="bar"></span><span class="bar"></span><span class="bar"></span></span></a>
-				<ul>
-					<li><a href="">About Us</a></li>
-					<li><a href="">Products</a></li>
-					<li><a href="">FAQs</a></li>
-					<li><a href="">Contact Us</a></li>
-				</ul>
-			</nav>
-			
-		</div>
-	</header>
+  <header class="header">
+    <div class="wrapper">
+    
+      <div class="logo">
+        <a href="./">Company Name</a>
+      </div>
+      
+      <nav class="main-nav">
+        <a href="" class="btn mobile-menu-btn">Menu <span class="icon-menu"><span class="bar"></span><span class="bar"></span><span class="bar"></span></span></a>
+        <ul>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">FAQs</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+      </nav>
+    
+    </div>
+  </header>
