cd30aaa102842dfdd3e8601fd67b78151a8e5366
Brad Sawicki <git@bradsawicki.com>
2014-08-27 09:56:03 -0500

    Update gem compass version / Update sass directory names

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <div class="hero">
     <h1 class="title">Save the Day!</h1>
     <p>Donec tristique, felis hendrerit blandit consectetur, lectus risus semper nibh, ac faucibus ligula diam a turpis. Nunc ac dapibus nunc, sit amet posuere erat.</p>
-    <p><a href="" class="btn white">Learn More</a></p>
+    <p><a href="" class="btn btn-white">Learn More</a></p>
   </div>
     
   <section id="activities" class="activities">
