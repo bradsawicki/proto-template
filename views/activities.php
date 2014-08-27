f5c7adde5ce921b4ea562e89b9d4a5f07408ab47
Brad Sawicki <git@bradsawicki.com>
2014-08-27 15:41:01 -0500

    section views added

---
+++ views/activities.php
@@ -0,0 +1,41 @@
+<section id="activities" class="activities">
+  <div class="wrapper">
+	    
+    <div class="grid">
+    
+      <article class="col-1-4 news-post">
+        <div class="post-thumb"><img src="/assets/images/img-300-01.jpg"></div>
+        <div class="post-info">
+          <p class="post-date">Jun 11, 2014</p>
+          <h3 class="post-title">Big news is happening every day!</h3>
+        </div>
+      </article>
+      
+      <article class="col-1-4 news-post">
+        <div class="post-thumb"><img src="/assets/images/img-300-02.jpg"></div>
+        <div class="post-info">
+          <p class="post-date">Jun 11, 2014</p>
+          <h3 class="post-title">Big news is happening every day!</h3>
+        </div>
+      </article>
+      
+      <article class="col-1-4 news-post">
+        <div class="post-thumb"><img src="/assets/images/img-300-03.jpg"></div>
+        <div class="post-info">
+          <p class="post-date">Jun 11, 2014</p>
+          <h3 class="post-title">Big news is happening every day!</h3>
+        </div>
+      </article>
+      
+      <article class="col-1-4 news-post">
+        <div class="post-thumb"><img src="/assets/images/img-300-04.jpg"></div>
+        <div class="post-info">
+          <p class="post-date">Jun 11, 2014</p>
+          <h3 class="post-title">Big news is happening every day!</h3>
+        </div>
+      </article>
+    
+    </div>
+
+  </div>
+</section>
---
+++ views/hero.php
@@ -0,0 +1,5 @@
+<div class="hero">
+  <h1 class="title">Save the Day!</h1>
+  <p>Donec tristique, felis hendrerit blandit consectetur, lectus risus semper nibh, ac faucibus ligula diam a turpis. Nunc ac dapibus nunc, sit amet posuere erat.</p>
+  <p><a href="" class="btn btn-white">Learn More</a></p>
+</div>
