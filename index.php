067fd5e1565c9d944ef6e27b536e8728218a0e26
Brad Sawicki <git@bradsawicki.com>
2014-08-27 10:02:17 -0500

    Add new Mixins / Break sections into views

--- index.php
+++ index.php
@@ -1,51 +1,7 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
-	
-  <div class="hero">
-    <h1 class="title">Save the Day!</h1>
-    <p>Donec tristique, felis hendrerit blandit consectetur, lectus risus semper nibh, ac faucibus ligula diam a turpis. Nunc ac dapibus nunc, sit amet posuere erat.</p>
-    <p><a href="" class="btn btn-white">Learn More</a></p>
-  </div>
-    
-  <section id="activities" class="activities">
-    <div class="wrapper">
-		    
-      <div class="grid">
-      
-        <article class="col-1-4 news-post">
-          <div class="post-thumb"><img src="/assets/images/img-300-01.jpg"></div>
-          <div class="post-info">
-            <p class="post-date">Jun 11, 2014</p>
-            <h3 class="post-title">Big news is happening every day!</h3>
-          </div>
-        </article>
-        
-        <article class="col-1-4 news-post">
-          <div class="post-thumb"><img src="/assets/images/img-300-02.jpg"></div>
-          <div class="post-info">
-            <p class="post-date">Jun 11, 2014</p>
-            <h3 class="post-title">Big news is happening every day!</h3>
-          </div>
-        </article>
-        
-        <article class="col-1-4 news-post">
-          <div class="post-thumb"><img src="/assets/images/img-300-03.jpg"></div>
-          <div class="post-info">
-            <p class="post-date">Jun 11, 2014</p>
-            <h3 class="post-title">Big news is happening every day!</h3>
-          </div>
-        </article>
-        
-        <article class="col-1-4 news-post">
-          <div class="post-thumb"><img src="/assets/images/img-300-04.jpg"></div>
-          <div class="post-info">
-            <p class="post-date">Jun 11, 2014</p>
-            <h3 class="post-title">Big news is happening every day!</h3>
-          </div>
-        </article>
-      
-      </div>
 
-  </div>
-</section>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/hero.php'; ?>
+    
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/activities.php'; ?>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
